47a481d887beb2fe8f54611a7de04e9752b2e33b
willy <willy.martin@edu.ece.fr>
2019-04-15 14:19:41 +0200

    creation kruskal.c

---
+++ projet_piscine/kruskal.cpp
@@ -0,0 +1,17 @@
+//
+// Created by willy on 15/04/2019.
+//
+
+//!
+//! \file kruskal.c
+//! \brief
+//! \authors BRIONNE,MARTIN,SIROT
+//! \version 0.01
+//! \date 15 avril 2019
+//! \return
+
+
+std::pair<Arete*,boolean> prim(int num_pond)
+{
+
+}
--- projet_piscine/main.cpp
+++ projet_piscine/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+//!
+//! \file main.c
+//! \brief
+//! \authors BRIONNE,MARTIN,SIROT
+//! \version 0.01
+//! \date 15 avril 2019
+//! \return
+
+
+int main();
+
 int main() {
     std::cout << "Hello c'est willy !!!" << std::endl;
     return 0;
 }
+
